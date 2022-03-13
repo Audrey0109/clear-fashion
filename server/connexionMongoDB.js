a0aa04ae65a31a2171073831acfd52bcafc7308e
unknown <adurey.chmiel@hotmail.fr>
2022-03-13 17:38:48 +0100

    test db

--- server/connexionMongoDB.js
+++ server/connexionMongoDB.js
@@ -1,7 +1,6 @@
 const {MongoClient} = require('mongodb');
-const MONGODB_DB_NAME = 'clearfashion';
+const MONGODB_DB_NAME = 'AudreyCHMIEL';
 const MONGODB_URI = "mongodb+srv://user:web_clear_fashion@clearfashion.biwwt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-
 
 async function connect() {
     try 
