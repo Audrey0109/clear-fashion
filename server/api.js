2786da95bca277a8cae1b6f218554bb80bdfdb42
unknown <adurey.chmiel@hotmail.fr>
2022-03-15 16:36:09 +0100

    creation of endpoints td5

--- server/api.js
+++ server/api.js
@@ -2,6 +2,7 @@ const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
 
+const db= require('./db');
 const PORT = 8092;
 
 const app = express();
@@ -16,6 +17,41 @@ app.options('*', cors());
 
 app.get('/', (request, response) => {
   response.send({'ack': true});
+});
+
+/// ENDPOINTS TO IMPLEMENT
+//Products id
+app.get('/products/:id', async (request, response) => {
+  wanted_id=request.params.id;
+  result=await db.find({_id:wanted_id});
+  console.log(result);
+  response.send(result);
+});
+
+//products search
+app.get('/products/search/', async (request, response) => {
+  var url=new URL('http://localhost:8092'+request.originalUrl);
+  console.log('search params:',url.searchParams)
+  query={}
+  //brand
+  if(url.searchParams.has('brand')){
+    console.log('has brand')
+    query['brand']=url.searchParams.get('brand');
+  }
+  //price
+  if(url.searchParams.has('price')){
+    console.log('has price')
+    query['price']=parseInt(url.searchParams.get('price'));  }
+      //limit
+  if(url.searchParams.has('limit')){
+    console.log('has limit')
+    //default is 12
+    var wanted_limit=parseInt(url.searchParams.get('limit'));  }
+  console.log("query:",query);
+  result=await db.find(query,0,limit=wanted_limit);
+  //result=String(result);
+  //console.log(result);
+  response.send(result);
 });
 
 app.listen(PORT);
